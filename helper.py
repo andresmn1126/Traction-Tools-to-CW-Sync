7a1651ce2b588968f18baec1dfc50c1b1d29d7a1
Andres Martinez <amartinez@andymartinez.tech>
2021-05-06 15:21:06 -0400

    Modified get_todos function to use id generated by API call in preparation for being able to pull the todos for all users, added filter to only pull todos beginging with CW

--- helper.py
+++ helper.py
@@ -2,5 +2,5 @@ def shorten_name(str):
     l = str.split()
     first_name = l[0]
     last_name = l[1]
-    new = first_name[0].upper() + last_name.title()
+    new = first_name[0].lower() + last_name.lower()
     return new   
--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import requests
 import json
 import base64
 import datetime
-import helper
+
+from requests.api import head
+from helper import shorten_name
 
 with open('creds.json') as json_creds:
     creds = json.load(json_creds)
@@ -41,22 +43,32 @@ def get_tt_userids(token):
                'Authorization':bearer_token
                }
     r = requests.get(url, headers=headers).json()
-    tt_userids = [{user['Name']: user['Id']}]
+    tt_userids = [{"name": shorten_name(user['Name']), "ttid": user['Id']} for user in r]
+    return tt_userids
 
-def get_personal_todos(token):
-    url = "https://traction.tools/api/v1/todo/users/mine"
+def get_personal_todos(token, tt_dict):
     bearer_token = f'Bearer {token}'
     headers = {'Accept': 'application/json',
-               'Authorization':bearer_token
+               'Authorization': bearer_token
                }
-    r = requests.get(url, headers=headers).json()
-    todos = [todo['Name'] for todo in r ]
-    return todos
+    all_todos = []
+    for user in tt_dict:
+        if user['name'] == 'amartinez':
+            url = f"https://traction.tools/api/v1/todo/user/{user.get('ttid')}"
+            r = requests.get(url, headers=headers).json()
+            todos = [todo['Name'] for todo in r if 'CW' in todo['Name'][:2]]
+            user['todos'] = todos
+            all_todos.append(user)
+    return all_todos        
 
 def get_cw_members():
-    pass
-
-
+    endpoint = "/system/members"
+    params = {"pageSize": 1000,
+              "field": "id,identifier"
+             }
+    r = requests.get(cwurl + endpoint, params=params, headers=cwheaders).json()
+    cw_members = [{"name": member['identifier'], "cwid": member['id']} for member in r]
+    return cw_members
 
 def get_cw_active_activities():
     endpoint = "/sales/activities"
@@ -93,4 +105,7 @@ activities = get_cw_active_activities()
 post_cw_activities(activities, todos)
 '''
 
-print(shorten_name("Andres Martinez"))
+token = get_token()
+tt_ids = get_tt_userids(token)
+todos = get_personal_todos(token, tt_ids)
+print(todos)
